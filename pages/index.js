397262e38cb04e5ba43ce990b5c23485c5a80104
João Vitor Vian <joaovitor_vian@hotmail.com>
2023-07-27 00:52:33 +0000

    feat: add coming soon section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
 function Home() {
-  return <h1>É sobre isso e ta tudo bem.</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <div
+        className="coming-soon-container"
+        style={{
+          textAlign: "center",
+          border: "1px solid #808080",
+          borderRadius: "10px",
+          width: "30%",
+          height: "fit-content",
+        }}
+      >
+        <h1 className="title">Em construção</h1>
+        <p className="text">
+          Um lugar para compartilhar minhas idéias, <br />
+          divulgar aprendizados, <br />
+          me conectar com meus iguais.
+        </p>
+      </div>
+
+      <div className="about"></div>
+    </div>
+  );
 }
 
 export default Home;
