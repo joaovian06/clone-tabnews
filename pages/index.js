6383edf727c211d56619c0f1276b7c48a34575a0
João Vitor Vian <joaovitor_vian@hotmail.com>
2023-07-27 01:01:03 +0000

    refactor: move coming soon section to component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import ComingSoon from "../src/components/ComingSoon";
+
 function Home() {
   return (
     <div
@@ -9,24 +11,7 @@ function Home() {
         alignItems: "center",
       }}
     >
-      <div
-        className="coming-soon-container"
-        style={{
-          textAlign: "center",
-          border: "1px solid #808080",
-          borderRadius: "10px",
-          width: "30%",
-          height: "fit-content",
-        }}
-      >
-        <h1 className="title">Em construção</h1>
-        <p className="text">
-          Um lugar para compartilhar minhas idéias, <br />
-          divulgar aprendizados, <br />
-          me conectar com meus iguais.
-        </p>
-      </div>
-
+      <ComingSoon></ComingSoon>
       <div className="about"></div>
     </div>
   );
