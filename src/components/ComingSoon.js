4550052f22c68a1ccaf4404b755782c3547b6990
João Vitor Vian <joaovitor_vian@hotmail.com>
2023-08-11 19:30:06 +0000

    feat: add coming soon css module

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,15 +1,8 @@
+import styles from "../stylessheets/ComingSoon.module.scss";
+
 export default function ComingSoon() {
   return (
-    <div
-      className="coming-soon-container"
-      style={{
-        textAlign: "center",
-        border: "1px solid #808080",
-        borderRadius: "10px",
-        width: "30%",
-        height: "fit-content",
-      }}
-    >
+    <div className={styles.ComingSoonContainer}>
       <h1 className="title">Em construção</h1>
       <p className="text">
         Um lugar para compartilhar minhas idéias, <br />
